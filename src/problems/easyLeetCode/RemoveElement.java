ef3e67a78d93679c873b29da23d29e7326b2eff8
Umut Bilal Okur <umutokur11@outlook.com>
2023-02-18 21:58:39 +0300

    solution
    removeElement solution

--- src/problems/easyLeetCode/RemoveElement.java
+++ src/problems/easyLeetCode/RemoveElement.java
@@ -3,20 +3,13 @@ package problems.easyLeetCode;
 public class RemoveElement
 {
     public int removeElement(int[] nums, int val) {
-        int k = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == val)
-            {
-                nums[i] = -1;
-                k++;
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
             }
         }
-
-        for (int i : nums) {
-            System.out.println(i);
-        }
-
-
-        return k;
+        return i;
     }
 }
