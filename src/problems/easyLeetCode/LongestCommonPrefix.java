5b7af1ce4003bc6217c4a970e29a24c404c540d6
Umut Bilal Okur <umutokur11@outlook.com>
2023-02-27 22:13:30 +0300

    Create LongestCommonPrefix.java

---
+++ src/problems/easyLeetCode/LongestCommonPrefix.java
@@ -0,0 +1,14 @@
+package problems.easyLeetCode;
+
+public class LongestCommonPrefix
+{
+    public String longestCommonPrefix(String[] str) {
+        int n = str.length;
+        int i = 0;
+        while(i < n)
+        {
+            for (int j = 0;j < n;j++)
+        }
+        return "";
+    }
+}
